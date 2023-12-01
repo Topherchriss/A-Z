f0da3e2f7b5d2a36237cb96ddcc3adb916f2beb0
Topherchriss <outlawsrabut@gmail.com>
2023-12-02 00:19:54 +0300

    json_utils doc strings

--- json_utils.py
+++ json_utils.py
@@ -2,6 +2,16 @@ import json
 from Alpha import BankAccount
 
 def save_bank_account_data(account, filename):
+     """
+    Save the data of a BankAccount instance to a JSON file.
+
+    Parameters:
+    - account (BankAccount): The BankAccount instance to save.
+    - filename (str): The name of the file to save the data to.
+
+    Returns:
+    None
+    """
     try:
         with open(filename, "w") as file:
             json.dump(account.to_json(), file, indent=2)
@@ -9,6 +19,17 @@ def save_bank_account_data(account, filename):
         print(f"An error occurred while saving data: {e}")
 
 def load_bank_account_data(filename):
+
+    """
+    Load data from a JSON file and create a BankAccount instance.
+
+    Parameters:
+    - filename (str): The name of the file to load data from.
+
+    Returns:
+    BankAccount or None: The loaded BankAccount instance, or None if an error occurred.
+    """
+
     try:
         with open(filename, "r") as file:
             data = json.load(file)
